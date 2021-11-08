3ced88f44bded6642c4d7e9fef16e4533522cb4e
steve <stevechoii92@gmail.com>
2021-11-08 16:18:55 +0900

    check

---
+++ reviews/management/commands/seed_reviews.py
@@ -0,0 +1,42 @@
+import random
+from django.core.management.base import BaseCommand
+from django_seed import Seed
+from reviews import models as review_models
+from users import models as user_models
+from rooms import models as room_models
+
+
+class Command(BaseCommand):
+
+    help = "This command tells me that he loves me"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--number",
+            default=2,
+            type=int,
+            help="How many times do you want me to tell you that I love you?",
+        )
+
+    def handle(self, *args, **options):
+
+        number = options.get("number")
+        seeder = Seed.seeder()
+        users = user_models.User.objects.all()
+        rooms = room_models.Room.objects.all()
+        seeder.add_entity(
+            review_models.Review,
+            number,
+            {
+                "accuracy": lambda x: random.randint(0, 5),
+                "communication": lambda x: random.randint(0, 5),
+                "cleanliness": lambda x: random.randint(0, 5),
+                "location": lambda x: random.randint(0, 5),
+                "check_in": lambda x: random.randint(0, 5),
+                "value": lambda x: random.randint(0, 5),
+                "user": lambda x: random.choice(users),
+                "room": lambda x: random.choice(rooms),
+            },
+        )
+        seeder.execute()
+        self.stdout.write(self.style.SUCCESS(f"{number} reviews created!"))
---
+++ rooms/views.py
@@ -0,0 +1,23 @@
+from django.views.generic import ListView
+from django.urls import reverse
+from django.shortcuts import render, redirect
+from . import models
+
+# Create your views here.
+class HomeView(ListView):
+
+    """HomeView Definition"""
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
+
+
+def room_detail(request, pk):
+    try:
+        room = models.Room.objects.get(pk=pk)
+        return render(request, "rooms/detail.html", {"room": room})
+    except models.Room.DoesNotExist:
+        return redirect(reverse("core:home"))
